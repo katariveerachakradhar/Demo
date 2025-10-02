1c16b89e593444a1dee7b7108c386bd7df680606
Veera <veeravamshi1@gmail.com>
2025-10-02 10:55:08 +0530

    Shell EC2

--- Shell-EC2.sh
+++ Shell-EC2.sh
@@ -5,14 +5,14 @@ source ./variables.sh
 echo "Launching EC2 instance"
 
 aws ec2 run-instance\
-    image-id="$AMI_ID" \
-    count="$COUNT" \
-    instance-type="$INSTANCE_TYPE"\
-    security-group="$SECURITY_GROUP_IDS" \
-    subnet-id="$SUBNET_ID" \
-    tag-specifications "ResouceType=instance, Tags=[{value=$TAG_NAME}]"\
-    region "$REGION" \
-    output json
+image-id="$AMI_ID" \
+count="$COUNT" \
+instance-type="$INSTANCE_TYPE"\
+security-group="$SECURITY_GROUP_IDS" \
+subnet-id="$SUBNET_ID" \
+tag-specifications "ResouceType=instance, Tags=[{value=$TAG_NAME}]"\
+region "$REGION" \
+output json
     
 echo "EC2 launched"
 
